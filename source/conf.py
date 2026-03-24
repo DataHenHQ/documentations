8053ec7e4ea4a0a76f16c5fd07475a566d4f0a46
Rony Sandoval <jr.potit@gmail.com>
2026-03-24 11:57:41 -0600

    Configure Sphinx to suppress build warnings and add static files path
    Added fail_on_warning: false to .readthedocs.yaml to prevent build failures from warnings. Added html_static_path configuration in conf.py to support custom static files.

--- source/conf.py
+++ source/conf.py
@@ -47,6 +47,11 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
